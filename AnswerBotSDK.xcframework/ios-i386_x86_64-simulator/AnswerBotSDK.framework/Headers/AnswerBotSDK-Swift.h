a436a69fbd0d37983e108dbf69cea685336cdbb0
i18nbot <hcdev@zendesk.com>
2020-06-08 08:50:57 +0000

    release/2.0.5

--- AnswerBotSDK.xcframework/ios-i386_x86_64-simulator/AnswerBotSDK.framework/Headers/AnswerBotSDK-Swift.h
+++ AnswerBotSDK.xcframework/ios-i386_x86_64-simulator/AnswerBotSDK.framework/Headers/AnswerBotSDK-Swift.h
@@ -190,6 +190,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 @import AnswerBotProvidersSDK;
 @import ObjectiveC;
+@import SupportProvidersSDK;
 @import UIKit;
 #endif
 
@@ -215,6 +216,8 @@ SWIFT_CLASS_NAMED("AnswerBotEngine")
 - (nonnull instancetype)init SWIFT_UNAVAILABLE;
 + (nonnull instancetype)new SWIFT_UNAVAILABLE_MSG("-init is unavailable");
 @end
+
+
 
 
 
@@ -424,6 +427,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 @import AnswerBotProvidersSDK;
 @import ObjectiveC;
+@import SupportProvidersSDK;
 @import UIKit;
 #endif
 
@@ -449,6 +453,8 @@ SWIFT_CLASS_NAMED("AnswerBotEngine")
 - (nonnull instancetype)init SWIFT_UNAVAILABLE;
 + (nonnull instancetype)new SWIFT_UNAVAILABLE_MSG("-init is unavailable");
 @end
+
+
 
 
 
--- ZendeskAnswerBotSDK.podspec
+++ ZendeskAnswerBotSDK.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "ZendeskAnswerBotSDK"
-  s.version      = "2.0.4"
+  s.version      = "2.0.5"
   s.summary      = "ZendeskAnswerBotSDK #{s.version.to_s}"
   s.homepage     = "https://developer.zendesk.com/embeddables"
   s.license      = {
@@ -21,6 +21,6 @@ Pod::Spec.new do |s|
   s.swift_version = '5.2.2'
   s.source       = { :git => "https://github.com/zendesk/answer_bot_sdk_ios.git", :tag => s.version }
   s.vendored_frameworks = 'AnswerBotSDK.framework'
-  s.dependency 'ZendeskAnswerBotProvidersSDK', '~> 2.0.4'
-  s.dependency 'ZendeskMessagingSDK', '~> 3.5.0'
+  s.dependency 'ZendeskAnswerBotProvidersSDK', '~> 2.0.5'
+  s.dependency 'ZendeskMessagingSDK', '~> 3.6.0'
 end
