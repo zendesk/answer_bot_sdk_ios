5bdd8c889b3ebcd0ed7d8044ca6257ab8c0c4026
Malek Trabelsi <malek.trabelsi@zendesk.com>
2022-03-23 13:51:52 +0100

    A workaround for Xcode 13.3 SPM dependencies install failure

--- Package.swift
+++ Package.swift
@@ -16,19 +16,19 @@ let package = Package(
     dependencies: [
         .package(name: "ZendeskAnswerBotProvidersSDK",
                  url: "https://github.com/zendesk/answer_bot_providers_sdk_ios",
-                 .exact("2.1.4")),
+                 .branch("bugfix/Xcode13.3-SPM-integration")),
         .package(name: "ZendeskMessagingSDK",
                  url: "https://github.com/zendesk/messaging_sdk_ios",
-                 .exact("3.8.3"))
+                 .branch("bugfix/Xcode13.3-SPM-integration"))
     ],
     targets: [
         .binaryTarget(
-            name: "ZendeskAnswerBotSDK",
+            name: "AnswerBotSDK",
             path: "AnswerBotSDK.xcframework"
         ),
         .target(name: "ZendeskAnswerBotSDKTargets",
                 dependencies: [
-                    .target(name: "ZendeskAnswerBotSDK"),
+                    .target(name: "AnswerBotSDK"),
                     .product(name: "ZendeskAnswerBotProvidersSDK", package: "ZendeskAnswerBotProvidersSDK"),
                     .product(name: "ZendeskMessagingSDK", package: "ZendeskMessagingSDK")
                 ],
