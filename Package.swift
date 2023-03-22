5a01dff8d6c5303ce76707a51a777264953f3768
runner <runner@zendesk.com>
2023-03-22 10:28:16 +0000

    [RELEASE] New version 4.0.0

--- Package.swift
+++ Package.swift
@@ -16,10 +16,10 @@ let package = Package(
     dependencies: [
         .package(name: "ZendeskAnswerBotProvidersSDK",
                  url: "https://github.com/zendesk/answer_bot_providers_sdk_ios",
-                 .exact("3.0.0")),
+                 .exact("answer_bot_providers_sdk_ios_version")),
         .package(name: "ZendeskMessagingSDK",
                  url: "https://github.com/zendesk/messaging_sdk_ios",
-                 .exact("4.0.0"))
+                 .exact("messaging_sdk_ios_version"))
     ],
     targets: [
         .binaryTarget(
