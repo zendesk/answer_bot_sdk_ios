b251816ae50847159f3d578193b4419e218d9821
GaborVass <94388023+gaborvass-zendesk@users.noreply.github.com>
2023-03-22 14:35:20 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,10 +16,10 @@ let package = Package(
     dependencies: [
         .package(name: "ZendeskAnswerBotProvidersSDK",
                  url: "https://github.com/zendesk/answer_bot_providers_sdk_ios",
-                 .exact("answer_bot_providers_sdk_ios_version")),
+                 .exact("4.0.0")),
         .package(name: "ZendeskMessagingSDK",
                  url: "https://github.com/zendesk/messaging_sdk_ios",
-                 .exact("messaging_sdk_ios_version"))
+                 .exact("5.0.0"))
     ],
     targets: [
         .binaryTarget(
